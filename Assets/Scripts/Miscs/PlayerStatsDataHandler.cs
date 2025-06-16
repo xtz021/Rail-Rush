94c4ef9fb88b23fb53003086903b6d1a8bbfdea8
xtz021 <xaviertz021@gmail.com>
2025-06-16 21:10:14 +0700

    Added PlayerStatsUI
    Added PlayerStatsUI (not finished)

---
+++ Assets/Scripts/Miscs/PlayerStatsDataHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStatsDataHandler : MonoBehaviour
+{
+    [Header("Stats UI Text")]
+    [SerializeField] TMP_Text tmp_BestRun;
+    [SerializeField] TMP_Text tmp_NuggetsCollected;
+    [SerializeField] TMP_Text tmp_MaxNuggetsCollectedInAGame;
+    [SerializeField] TMP_Text tmp_TotalPlay;
+    [SerializeField] TMP_Text tmp_TotalMissionCompleted;
+    [Space(20f)]
+    [SerializeField] TMP_Text tmp_Deaths;
+    [SerializeField] TMP_Text tmp_DeathsByUpperObs;
+    [SerializeField] TMP_Text tmp_DeathsByLowerObs;
+    [SerializeField] TMP_Text tmp_DeathsByRightObs;
+    [SerializeField] TMP_Text tmp_DeathsByleftObs;
+    [Space(20f)]
+    [SerializeField] TMP_Text tmp_AmethystCollected;
+    [SerializeField] TMP_Text tmp_GarnetCollected;
+    [SerializeField] TMP_Text tmp_TopazCollected;
+    [SerializeField] TMP_Text tmp_SpinelCollected;
+    [SerializeField] TMP_Text tmp_RubyCollected;
+    [SerializeField] TMP_Text tmp_SapphireCollected;
+    [SerializeField] TMP_Text tmp_EmeraldCollected;
+    [SerializeField] TMP_Text tmp_DiamondCollected;
+
+}
+
+public class PlayerStats
+{
+    int BestRun;
+    int NuggetsCollected;
+
+}
