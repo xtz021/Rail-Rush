b8801a4ac9b6815018434d39e716eddc0217b9a5
XavierTZ <xaviertz021@gmail.com>
2025-12-14 21:31:24 +0700

    Added Object Pooling script
    Added Object Pooling script in MyPool

---
+++ Assets/Scripts/Miscs/ObjectsPooling/MyPool.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPool
+{
+    private Stack<GameObject> poolStack = new Stack<GameObject>();
+    private GameObject baseGO;
+    private GameObject tmp;
+    private ReturnToMyPool returnPool;
+
+    public MyPool(GameObject baseGO)
+    {
+        this.baseGO = baseGO;
+    }
+
+    public GameObject Get()
+    {
+        if (poolStack.Count > 0)
+        {
+            tmp = poolStack.Pop();
+            tmp.SetActive(true);
+        }
+        else
+        {
+            tmp = GameObject.Instantiate(baseGO);
+            returnPool = tmp.AddComponent<ReturnToMyPool>();
+            returnPool.myPool = this;
+        }
+        return tmp;
+    }
+
+    public void AddToPool(GameObject go)
+    {
+        go.SetActive(false);
+        poolStack.Push(go);
+    }
+}
+
+public class ReturnToMyPool : MonoBehaviour
+{
+    public MyPool myPool;
+
+    public void OnDisable()
+    {
+        if (myPool != null)
+        {
+            myPool.AddToPool(gameObject);
+        }
+    }
+}
